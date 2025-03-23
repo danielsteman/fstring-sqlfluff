4910a6881d7a5d8f4096f44cfad9d7ec214c6f92
Daniel Steman <daniel-steman@live.nl>
2025-03-23 21:27:46 +0100

    wip

--- fstringsqlfluff/sql_linter.py
+++ fstringsqlfluff/sql_linter.py
@@ -1,28 +1,12 @@
 from dataclasses import dataclass
 from typing import Any
 from sqlfluff.core import Linter
-
-
-@dataclass
-class LintResult:
-    fixed_sql_code: str
-    violations: list[dict[str, Any]]
+import sqlfluff
 
 
 class SQLLinter:
     def __init__(self, dialect: str = "databricks"):
         self.linter = Linter(dialect=dialect)
 
-    def lint_sql(self, sql_code: str) -> LintResult:
-        """Lints the given SQL code and returns a list of violations."""
-        result = self.linter.lint_string(sql_code)
-        violations = [
-            {
-                "line": v.line_no,
-                "column": v.line_pos,
-                "description": v.desc,
-            }
-            for v in result.get_violations()
-        ]
-        fixed_sql_code = result.fix_string()
-        return LintResult(fixed_sql_code=fixed_sql_code, violations=violations)
+    def lint_sql(self, sql_code: str) -> str:
+        return sqlfluff.fix(sql_code)
--- tests/test_sql_linter.py
+++ tests/test_sql_linter.py
@@ -9,18 +9,17 @@ def linter():
 
 def test_lint_sql(linter):
     sql_code = """
-SELECT
-    a,
-    b
-FROM my_table
-WHERE id = 1 LIMIT 10
+SELeCT
+a,
+b,
+c FROM my_table WHeRE id = 1 LIMIT 10
     """
     result = linter.lint_sql(sql_code)
-    violations = result.violations
-    print(violations)
-    assert isinstance(violations, list)
-    for v in violations:
-        assert isinstance(v, dict)
-        assert "line" in v
-        assert "column" in v
-        assert "description" in v
+    expected = """SELECT
+    a,
+    b,
+    c
+FROM my_table
+WHERE id = 1 LIMIT 10
+"""
+    assert result == expected
